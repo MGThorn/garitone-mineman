6593b5ab53be3292be54cf0adeaa9699776a3395
MGThornHomePC <Lennart.Dorn1@outlook.de>
2026-07-18 21:01:39 +0200

    implemented auto eat and easy eat feature + settings

--- src/client/java/com/example/client/ExampleModClient.java
+++ src/client/java/com/example/client/ExampleModClient.java
@@ -1,10 +1,14 @@
 package com.example.client;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import fi.dy.masa.malilib.event.InitializationHandler;
+import com.example.client.feature.EasyEatHandler;
 
 public class ExampleModClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+		InitializationHandler.getInstance().registerInitializationHandler(new InitHandler());
+		ClientTickEvents.START_CLIENT_TICK.register(mc -> EasyEatHandler.onClientTick());
 	}
 }
